28c079adeae68554853167f85bb89b81c5510089
Divya Rana <divyarana@Divyas-MacBook-Air.local>
2023-12-13 16:25:24 +0100

    updated

--- distort.py
+++ distort.py
@@ -230,7 +230,7 @@ if __name__ == "__main__":
         #weeding out to the strong lensing systems and foreground sources configuration
             if (sflag[jj]!=1.0) and (etan[jj]==0.0):
                 continue
-            fdata.write('%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\n'%(lra[ii], ldec[ii], lzred[ii], logmstel[ii], logmh[ii], ss.conc, sra[jj], sdec[jj], szred, s1[jj], s2[jj], etan[jj], et[jj], ex[jj], proj_sep[jj]))
+            fdata.write('%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\n'%(lra[ii], ldec[ii], lzred[ii], logmstel[ii], logmh[ii], ss.conc, sra[jj], sdec[jj], szred[jj], s1[jj], s2[jj], etan[jj], et[jj], ex[jj], proj_sep[jj]))
 
     fdata.close()
     comm.Barrier()
