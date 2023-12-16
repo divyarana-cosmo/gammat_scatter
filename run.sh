ce331c9f0e7f7289d4f74314c2082f8e837c8cd2
Divya Rana <rana@dobbe.strw.leidenuniv.nl>
2023-12-16 14:52:44 +0100

    updated

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 #!/bin/zsh
+
 mpirun -np 6 python distort.py --config config  --logmstelmin 11.0 --logmstelmax 11.3
 mpirun -np 6 python distort.py --config config  --logmstelmin 11.3 --logmstelmax 11.6
 mpirun -np 6 python distort.py --config config  --logmstelmin 11.6 --logmstelmax 14 #sort of everthing above 11.6
