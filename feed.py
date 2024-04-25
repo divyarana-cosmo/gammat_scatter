888d830a070f221481cb9ad36e9a0c7e88135188
Divya Rana <rana@dobbe.strw.leidenuniv.nl>
2024-04-25 13:17:05 +0200

    updated

--- feed.py
+++ feed.py
@@ -5,14 +5,13 @@ import numpy as np
 nbins = 3
 binedgs = 10.0 + 0.5*np.arange(nbins+1)
 
-#call("python simulate_aroundlens.py --config config_full  --no_shape_noise True --logmstelmin 11.6 --logmstelmax 14.0 --test_case True --seed 345 >> test_out.dat_noshp 2>&1 &", shell=1)
-#call("python simulate_aroundlens.py --config config_full  --logmstelmin 11.6 --logmstelmax 14.0 --test_case True --seed 456 --no_shear True >> test_out.dat 2>&1 &", shell=1)
-#call("python simulate_aroundlens.py --config config_full  --rot90 True --logmstelmin 11.6 --logmstelmax 14.0 --test_case True  --seed 456 --no_shear True >> test_out.dat_90 2>&1 &", shell=1)
-
-
-
 for ss in np.arange(10):
     call("python simulate_aroundlens.py --config config_full  --logmstelmin 11.6 --logmstelmax 14.0 --test_case True --seed %d >> test_out.dat_%d 2>&1 &"%(ss,ss), shell=1)
+
+
+
+
+
     #call("python simulate_aroundlens.py --config config_full --logmstelmin 11.6 --logmstelmax 14.0 --test_case True --use_shear True --seed %s >> test_out.dat_%d 2>&1 &"%(ss,ss), shell=1)
 #    call("python simulate_aroundlens.py --config config_full --rot90 True --logmstelmin 11.6 --logmstelmax 14.0 --test_case True --use_shear True --seed %s >> test_out.dat_90_%s 2>&1 &"%(ss,ss), shell=1)
 #
@@ -28,6 +27,11 @@ for ss in np.arange(10):
 #
 #    
 #    print("feeding pipiline: ", binedgs[bb], binedgs[bb+1])
+#call("python simulate_aroundlens.py --config config_full  --no_shape_noise True --logmstelmin 11.6 --logmstelmax 14.0 --test_case True --seed 345 >> test_out.dat_noshp 2>&1 &", shell=1)
+#call("python simulate_aroundlens.py --config config_full  --logmstelmin 11.6 --logmstelmax 14.0 --test_case True --seed 456 --no_shear True >> test_out.dat 2>&1 &", shell=1)
+#call("python simulate_aroundlens.py --config config_full  --rot90 True --logmstelmin 11.6 --logmstelmax 14.0 --test_case True  --seed 456 --no_shear True >> test_out.dat_90 2>&1 &", shell=1)
+
+
 
 
 #call("python simulate_aroundlens.py --config config_full --no_shape_noise True --logmstelmin 11.6 --logmstelmax 14.0 --test_case True --use_shear True>> test_out.dat_noshp 2>&1 &", shell=1)
