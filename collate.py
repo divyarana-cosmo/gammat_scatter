dea4b84930853ff036f1f707edb7e499da8a8fab
Divya Rana <rana@dobbe.strw.leidenuniv.nl>
2026-06-05 00:31:51 +0200

    updated

--- collate.py
+++ collate.py
@@ -6,13 +6,15 @@ sys.path.append('/net/dobbe/data2/github/gammat_scatter/model/')
 sys.path.append('/net/dobbe/data2/github/gammat_scatter/src/')
 from model import model
 from distort_com import simshear
+from scipy.interpolate import interp1d
 
-def get_meas(outdir, logMstelarr):
+def get_meas(outdir, logMstelarr, nover_samp=1):
+    # reading the data
     from glob import glob
-   
+    print(logMstelarr)   
     for logMstelmin, logMstelmax in zip(logMstelarr[:-1], logMstelarr[1:]):
         print(logMstelmin, logMstelmax)
-        #flist = glob(outdir + 'simed_sources.dat_lmstelmin_%2.2f_lmstelmax_%2.2f_with_shape_noise_w_jacks_jk_*'%(logMstelmin, logMstelmax) + '_fast')
+        #flist = glob(outdir + 'simed_sources.dat_lmstelmin_%2.2f_lmstelmax_%2.2f_with_shape_noise_using_shear_w_jacks_jk_*'%(logMstelmin, logMstelmax) + '_fast')
         flist = glob(outdir + 'simed_sources.dat_lmstelmin_%2.2f_lmstelmax_%2.2f_with_shape_noise_w_jacks_jk_*'%(logMstelmin, logMstelmax) + '_fast')
         Njacks = int(len(flist))
         print('Number of jackknifes samples', Njacks)
@@ -20,21 +22,36 @@ def get_meas(outdir, logMstelarr):
         rbin      = np.array([])
         dsigmaarr = np.array([])
         xdsigmaarr = np.array([])
+        inp_gammat_arr = np.array([])
+
+        
         for jk in range(Njacks):
+            stel_num =0
+            stel_den =0
+            avg_inv_sigc_num =  0
+            avg_inv_sigc_den =  0
+
+            file = outdir + 'simed_sources.dat_lmstelmin_%2.2f_lmstelmax_%2.2f_with_shape_noise_w_jacks_jk_%d'%(logMstelmin, logMstelmax, jk) + '_fast' 
+            data = pd.read_csv(file, delim_whitespace=1, skipfooter=1, engine='python') 
+            stel_num += data['22-dsigmat_inp_bary'].values[:]*data['27-sumd_dsigma_den']
+            stel_den += data['27-sumd_dsigma_den']
+            avg_inv_sigc_num += data['28-sumd_wls_by_sigmac']
+            avg_inv_sigc_den += data['14-sumd_wls']
+
             num=0; xnum=0; den=0
             for ii in range(Njacks): # leave out one jackknife region
                 if ii==jk :
                     continue
+                #file = outdir + 'simed_sources.dat_lmstelmin_%2.2f_lmstelmax_%2.2f_with_shape_noise_using_shear_w_jacks_jk_%d'%(logMstelmin, logMstelmax, ii) + '_fast' 
                 file = outdir + 'simed_sources.dat_lmstelmin_%2.2f_lmstelmax_%2.2f_with_shape_noise_w_jacks_jk_%d'%(logMstelmin, logMstelmax, ii) + '_fast' 
-                #0-rmin/2+rmax/2 1-gammat 2-gammatsq 3-sigma_gammat 4-SN_Errgammat 5-gammax 6-gammaxsq 7-sigma_gammax 8-SN_Errgammax 9-gammat_inp 10-gammat_inp_bary 11-gammat_inp_dm 12-sumd_wls 13-dsigma 14-dsigmasq 15-SN_Errdsigmat 16-dsigmax 17-dsigmaxsq 18-SN_Errdsigmax 19-dsigmat_inp 20-dsigmat_inp_bary 21-dsigmat_inp_dm 22-sumd_dsigma_wls 23-sumd_dsigma_num 24-sumd_dsigma_den
-                data = pd.read_csv(file, delim_whitespace=1) 
+                data = pd.read_csv(file, delim_whitespace=1, skipfooter=1, engine='python') 
                 #idx = data['0-rmin/2+rmax/2']*1e3 > 4.0
                 #data = data[idx]
-                
-                num +=data['23-sumd_dsigma_num'].values[:]
-                xnum+=data['24-sumd_dsigmax_num'].values[:]
-                den +=data['25-sumd_dsigma_den'].values[:]
-                rbin = data['0-rmin/2+rmax/2'].values[:]
+                num +=data['25-sumd_dsigma_num'].values[:]
+                xnum+=data['26-sumd_dsigmax_num'].values[:]
+                den +=data['27-sumd_dsigma_den'].values[:]
+                rbin = data['2-rmin/2+rmax/2'].values[:]
+                inp_gammat_arr = data['11-gammat_inp'].values[:]
             dsigmaarr  = np.append(dsigmaarr,num/den)
             xdsigmaarr = np.append(xdsigmaarr,xnum/den)
     
@@ -48,160 +65,235 @@ def get_meas(outdir, logMstelarr):
         # calculating the covariances
         for ii in range(len(rbin)):
             for jj in range(len(rbin)):
-                cov[ii,jj] = np.mean((dsigmaarr[:,ii] - dsigma[ii])*(dsigmaarr[:,jj] - dsigma[jj]))
+                cov[ii,jj]  = np.mean((dsigmaarr[:,ii] - dsigma[ii])*(dsigmaarr[:,jj] - dsigma[jj]))
                 xcov[ii,jj] = np.mean((xdsigmaarr[:,ii] - xdsigma[ii])*(xdsigmaarr[:,jj] - xdsigma[jj]))
-        
-        cov *=(Njacks -1) # correction for the jackknife
-        xcov *=(Njacks -1)
+        #correction for jackknife and oversampling factor        
+        cov  *=(nover_samp*(Njacks -1))         
+        xcov *=(nover_samp*(Njacks -1))
         
         dsigmaerr       = np.diag(cov)**0.5
         xdsigmaerr      = np.diag(xcov)**0.5
+
+        
+        #stellar and sigma c part
+        stel_dsigmaarr = stel_num/stel_den 
+        avg_inv_sigcarr    = avg_inv_sigc_num/avg_inv_sigc_den
+        avg_inv_sigcarrsq   = stel_den/avg_inv_sigc_den
+                         
         # saving the output
-        np.savetxt(outdir + './dsigma.dat_lmstelmin_%2.2f_lmstelmax_%2.2f'%(logMstelmin, logMstelmax), np.transpose([rbin, dsigma, dsigmaerr, xdsigma, xdsigmaerr]), header='Rp[h-1 Mpc] dsigma dsigmaerr xdsigma xdsigmaerr')
-        np.savetxt(outdir + './cov_dsigma.dat_lmstelmin_%2.2f_lmstelmax_%2.2f'%(logMstelmin, logMstelmax), cov)
-        np.savetxt(outdir + './xcov_dsigma.dat_lmstelmin_%2.2f_lmstelmax_%2.2f'%(logMstelmin, logMstelmax), xcov)
-        return 0
+        #np.savetxt(outdir + '/dsigma.dat_lmstelmin_%2.2f_lmstelmax_%2.2f_ovpsamp_100'%(logMstelmin, logMstelmax), np.transpose([rbin, dsigma, dsigmaerr, xdsigma, xdsigmaerr, stel_dsigmaarr, avg_inv_sigcarr, avg_inv_sigcarrsq]), header='Rp[h-1 Mpc] dsigma dsigmaerr xdsigma xdsigmaerr stel_dsigma avg_sigc avg_sigc_sq')
+        ##np.savetxt(outdir + '/dsigma.dat_lmstelmin_%2.2f_lmstelmax_%2.2f'%(logMstelmin, logMstelmax), np.transpose([rbin, dsigma, dsigmaerr, xdsigma, xdsigmaerr, stel_dsigmaarr, avg_inv_sigcarr, avg_inv_sigcarrsq]), header='Rp[h-1 Mpc] dsigma dsigmaerr xdsigma xdsigmaerr stel_dsigma avg_sigc avg_sigc_sq')
+        np.savetxt(outdir + '/cov_dsigma.dat_lmstelmin_%2.2f_lmstelmax_%2.2f'%(logMstelmin, logMstelmax), cov)
+        np.savetxt(outdir + '/xcov_dsigma.dat_lmstelmin_%2.2f_lmstelmax_%2.2f'%(logMstelmin, logMstelmax), xcov)
+    return 0
+
+def make_plots(pltdir, logMstelarr, Njacks=100):
+    #fixing the minimum projected separation to be atleast 2*Re
+    lens_metadata   = pd.read_csv('/net/dobbe/data2/github/gammat_scatter/lens_sample.dat', delim_whitespace=1)
+    #pushing the fitting xx,yy in an array
+    xfit = np.array([])
+    yfit = np.array([])
 
 
-
-def make_plots(outdir, logMstelarr):
+    # remove first radial bin
+    ss=1
+    ax = plt.subplot(3,3,ss)
+    colorcnt=0
+    inp_gammat_arr =0
     # dsigma plots
-    for logMstelmin, logMstelmax in zip(logMstelarr[:-1], logMstelarr[1:]):
-        rbins, dsigma, dsigmaerr, xdsigma, xdsigmaerr = np.loadtxt(outdir + './dsigma.dat_lmstelmin_%2.2f_lmstelmax_%2.2f'%(logMstelmin, logMstelmax), unpack=1)
-        cov  = np.loadtxt(outdir + './cov_dsigma.dat_lmstelmin_%2.2f_lmstelmax_%2.2f'%(logMstelmin, logMstelmax))
-        xcov = np.loadtxt(outdir + './xcov_dsigma.dat_lmstelmin_%2.2f_lmstelmax_%2.2f'%(logMstelmin, logMstelmax))
-        dsigmaerr   = np.diag(cov)**0.5        
-        xdsigmaerr  = np.diag(xcov)**0.5        
+    for ii, (logMstelmin, logMstelmax)in enumerate(zip(logMstelarr[:-1], logMstelarr[1:])):
+        if logMstelmin>9.5 and logMstelmin<10.5:
+            continue
+        if logMstelmin>10.5 and logMstelmin<11.4:
+            continue
+        if logMstelmin>11.4:
+            continue
+
+        outdir = 'output/desi_z_0.0_0.4/iso_centrals/%2.2f_%2.2f_seed_%d/'%(logMstelmin, logMstelmax, ii)
+        file = outdir + 'simed_sources.dat_lmstelmin_%2.2f_lmstelmax_%2.2f_with_shape_noise_w_jacks_jk_%d'%(logMstelmin, logMstelmax, 10) + '_fast' 
+        data = pd.read_csv(file, delim_whitespace=1) 
+        rbin = data['2-rmin/2+rmax/2'].values[:]
+        inp_gammat_arr = data['11-gammat_inp'].values[:]
+        from scipy.interpolate import interp1d
+        idx = np.isfinite(inp_gammat_arr)
+        interpfunc = interp1d(inp_gammat_arr[idx], rbin[idx], kind='cubic')
+        
+        try:
+            rpvt = interpfunc(0.3)
+        except ValueError:
+            rpvt = min(rbin)
+
+        rbins, dsigma, dsigmaerr, xdsigma, xdsigmaerr, stel_dsigma, avg_sigc, avg_sigc_sq = np.loadtxt(outdir + 'dsigma.dat_lmstelmin_%2.2f_lmstelmax_%2.2f_ovpsamp_100'%(logMstelmin, logMstelmax), unpack=1)
+
+        #rbins, dsigma, dsigmaerr, xdsigma, xdsigmaerr 
+        cov  = np.loadtxt(outdir + 'cov_dsigma.dat_lmstelmin_%2.2f_lmstelmax_%2.2f'%(logMstelmin, logMstelmax))
+
+
+        idx             = (lens_metadata['logMmin']==logMstelmin) &(lens_metadata['logMmax']==logMstelmax)
+        Rmin            = lens_metadata['re_50'].values[idx] * 2.5/1e3 # 1e3 factor to convert Mpc to kpc
+
+        
+        if Rmin<rbins[0]:
+            Rmin = rbins[0]
+            xfit = np.append(xfit, Rmin)
+            yfit = np.append(yfit, dsigma[0])
+        
+        else:
+            func = interp1d(rbins, dsigma, kind='cubic')
+            xfit = np.append(xfit, Rmin)
+            yfit = np.append(yfit, func(Rmin))
+
+
+        ##removing nans
+        #idx = np.isfinite(dsigma) & (rbins>np.min(rbins))
+        #rbins       =   rbins[idx]
+        #dsigma      =   dsigma[idx]
+
+        #cov         =   np.delete(cov, ~idx, axis=0)
+        #cov         =   np.delete(cov, ~idx, axis=1)
+        
+        #print(sum(idx), cov)
+        dsigmaerr   = np.diag(cov)**0.5         
  
-        plt.subplot(3,3,1)
-        icov  = np.linalg.inv(cov)#*(Njacks - len(rbin) -2)/(Njacks -1)
+        icov  = np.linalg.inv(cov)*(Njacks - len(rbins) -2)/(Njacks -1)
         snr = (np.dot(dsigma, np.dot(icov, dsigma)))**0.5
-        print('SNR',snr) 
-        plt.errorbar(rbins*1e3, dsigma, yerr=dsigmaerr, fmt='.', capsize=3 ,label='%2.2f-%2.2f'%(logMstelmin, logMstelmax))
+        interpfunc = interp1d(rbins,dsigma)
+        print(logMstelmin, logMstelmax, rpvt)
 
-        # model predictions for the test sample
+        #ax.plot(rpvt*1e3, interpfunc(rpvt), 'k.', zorder=20)
+        #print(dsigma)
+        ax.errorbar(rbins*1e3, dsigma, yerr=dsigmaerr, fmt='.', capsize=3 ,label='(%2.1f, %2.1f)'%(logMstelmin, logMstelmax), color='C%d'%colorcnt)
 
-        ss                  = simshear(H0=100, Om0=0.25)
-        lzred       =   0.22326649
-        logmstel    =   10.47462749
-        logre      =   -2.38450355
-        logmh       =   12.42861652
-        lconc       =   9.67904423
-        szred = 0.9
+        print(rbins)
+        print(dsigma)
+        print(dsigmaerr/dsigma * 100)
 
-        proj_sep            = rbins
-        sigma_crit_inv      = ss._get_sigma_crit_inv_scalar(lzred=lzred, szred=szred) 
-        esd_s, sigma_s      = ss._get_esd_s(logmstel, logre,proj_sep)
-        esd_dm, sigma_dm    = ss._get_esd_dm(logmh, lconc, proj_sep)
+        ## reading the best fit model predictions
+        #preddata    =   np.loadtxt('./model/output_mcmc_desi_runs/predfile_nfw_lmstelmin_%2.2f_lmstelmax_%2.2f.dat_full_desixeuclid_seed_%d_fixed_conc_14kdr3'%(logMstelmin, logMstelmax, ii))
+        #predesd    =   preddata[np.argmin(preddata[:,-1]),:-1]
+        #ax.plot(rbins*1e3, predesd, 'C%d'%colorcnt)    
 
-        esd = (esd_s+esd_dm)/1e12
-        esd = esd/(1-(sigma_s + sigma_dm)*sigma_crit_inv)
 
-        ## for the test case 
-        #H0          =   100
-        #Om0         =   0.25
-        #lenstype    =   'test_desi'    
-        #logMmin     =   9.5
-        #logMmax     =   11.0
-        #zlmin       =   0.1
-        #zlmax       =   0.4
-        #Njacks      =   50
-        #zdiff       =   0.1
 
-        #mm = model(H0, Om0, lenstype, logMmin, logMmax, zlmin, zlmax, Njacks, zdiff)
+        colorcnt +=1
+    
 
-        #logmstel    =   10.47462749
-        #log_re      =   -2.38450355
-        #logmh       =   12.42861652
-        #cfac        =   1.0
+    # make sure x values are sorted
+    xfill = xfit * 1e3
+    idx = np.argsort(xfill)
+    xfill = xfill[idx]
+    yfill = yfit[idx]
+    
+    ax = plt.gca()
+    ymax = np.max(dsigma) * 3  # or any suitably large value
+    
+    ax.plot(xfill, yfill, '--', color='grey', zorder=10, label=r'$2.5 R_{\rm e}$')
 
-        #x = [logmstel, log_re, logmh, cfac]
+    xshade = np.insert(xfill, 0, 5)
+    yshade = np.insert(yfill, 0, yfill[0])  # horizontal extension
+    
+    ax.fill_between(
+        xshade,
+        yshade,
+        ymax,
+        color='grey',
+        alpha=0.5,
+        zorder=10
+    )
 
-        #red_esd     = mm.esd( x, rbins)
-        #gamma_esd   = mm.esd( x, rbins, reduced=False)
 
-        flist = outdir + 'simed_sources.dat_lmstelmin_%2.2f_lmstelmax_%2.2f_with_shape_noise_w_jacks_jk_0'%(logMstelmin, logMstelmax) + '_fast'
-        df= pd.read_csv(flist, delim_whitespace=1) 
-        meas_inp = df['19-dsigmat_inp'].values[:]
-        #plt.plot(rbins*1e3, df['19-dsigmat_inp'].values[:], '--r',label='input')
-        plt.plot(rbins*1e3, esd,':k', label='total')
-        #plt.plot(rbins*1e3, mm.esd_s/1e12, label='stellar')
-        #plt.plot(rbins*1e3, mm.esd_dm/1e12, label='dark matter')
+    #plt.plot(xfit*1e3, yfit, '--', color='grey', zorder=10)
+    #plt.xlim(5, 150)
+    plt.ylabel(r'$\Delta \Sigma[{\rm h M_{\odot} pc^{-2}}]$' )
+    plt.xlabel(r'${\rm R_{\rm p} [h^{-1}kpc]}$')
+    plt.xscale('log')
+    plt.yscale('log')
+    leg = plt.legend(fontsize='xx-small')
+    leg.set_zorder(100)
 
-        plt.legend()
-        #plt.xlim(2.0,5)
-        #plt.ylim(200,1000)
-        plt.ylabel(r'$\Delta \Sigma[{\rm h M_{\odot} pc^{-2}}]$' )
-        plt.xlabel(r'${\rm R_{\rm p} [h^{-1}kpc]}$')
-        plt.xscale('log')
-        plt.yscale('log')
+    plt.savefig(pltdir + 'dsigma-signals_paper.pdf')
+    return 0
+
+def plot_snr(pltdir, logMstelarr, Njacks=100):
+    #fixing the minimum projected separation to be atleast 2*Re
+    lens_metadata   = pd.read_csv('/net/dobbe/data2/github/gammat_scatter/lens_sample.dat', delim_whitespace=1)
+
+    # dsigma plots
+    logMstel_arr =   np.array([])
+    snr_arr      =   np.array([])
+
+    for ii, (logMstelmin, logMstelmax)in enumerate(zip(logMstelarr[:-1], logMstelarr[1:])):
+        print(logMstelmin, logMstelmax)
+        outdir = 'output/desi_z_0.0_0.4/iso_centrals/%2.2f_%2.2f_seed_%d/'%(logMstelmin, logMstelmax, ii)
+        #rbins, dsigma, dsigmaerr, xdsigma, xdsigmaerr = np.loadtxt(outdir + 'dsigma.dat_lmstelmin_%2.2f_lmstelmax_%2.2f'%(logMstelmin, logMstelmax), unpack=1)
+
+        rbins, dsigma, dsigmaerr, xdsigma, xdsigmaerr, stel_dsigma, avg_sigc, avg_sigc_sq = np.loadtxt(outdir + 'dsigma.dat_lmstelmin_%2.2f_lmstelmax_%2.2f_ovpsamp_100'%(logMstelmin, logMstelmax), unpack=1)
+
+        cov  = np.loadtxt(outdir + 'cov_dsigma.dat_lmstelmin_%2.2f_lmstelmax_%2.2f'%(logMstelmin, logMstelmax))
+        cov  = np.diag(np.diag(cov))
         
 
-        plt.subplot(3,3,2)
-        #plt.plot(rbins*1e3, (esd-dsigma)/dsigmaerr,'-k')
-        plt.plot(rbins*1e3, (esd-dsigma)/dsigmaerr,'-k')
-        plt.plot(rbins*1e3, (meas_inp-dsigma)/dsigmaerr,'-r')
-        plt.axhline(0.0,ls='--',color='grey')
-        plt.legend()
-        plt.ylabel(r'(inp-meas)/error' )
-        #plt.ylabel(r'(inp-meas)' )
-        plt.xlabel(r'${\rm R_{\rm p} [h^{-1}kpc]}$')
-        plt.xscale('log')
+
+        #removing nans
+        idx       = (lens_metadata['logMmin']==logMstelmin) &(lens_metadata['logMmax']==logMstelmax)
+        Rmin      = lens_metadata['re_50'].values[idx] * 2.5/1e3 # 1e3 factor to convert Mpc to kpc
+
+
+        idx         = (rbins>Rmin)
+        rbins       =   rbins[idx]
+        dsigma      =   dsigma[idx]
+
+        cov         =   np.delete(cov, ~idx, axis=0)
+        cov         =   np.delete(cov, ~idx, axis=1)
+        
+        #print(sum(idx), cov)
+        dsigmaerr   = np.diag(cov)**0.5         
+
+        #print(logMstelmin, logMstelmax, dsigmaerr/dsigma * 100)
+        #continue
  
+        icov  = np.linalg.inv(cov)*(Njacks - len(rbins) -2)/(Njacks -1)
+        snr = (np.dot(dsigma, np.dot(icov, dsigma)))**0.5
+        print(snr)
+        logMstel_arr =   np.append(logMstel_arr, [logMstelmin*0.5 + logMstelmax*0.5])
+        snr_arr      =   np.append(snr_arr     , [snr])
 
-    plt.legend(fontsize='xx-small')
-    plt.tight_layout()
-    plt.savefig(outdir + 'dsigma-signals-fast.pdf', dpi=300)
-    #plt.clf()
-    ## covariance plots
-    #for logMstelmin, logMstelmax in zip(logMstelarr[:-1], logMstelarr[1:]):
-    #    cov  = np.loadtxt(outdir + './cov_dsigma.dat_lmstelmin_%2.2f_lmstelmax_%2.2f'%(logMstelmin, logMstelmax))
-    #    corr = 0.0*cov
-    #    for ii in range(len(cov[0,:])):
-    #        for jj in range(len(cov[0,:])):
-    #            corr[ii,jj] = cov[ii,jj]/(cov[ii,ii]*cov[jj,jj])**0.5
 
-    #    plt.subplot(3,3,1)
-    #    plt.imshow(corr, origin='lower', vmin=-1, vmax=1)
-    #    plt.colorbar()
-    #    plt.ylabel(r'$\Delta \Sigma(R_{p, i})$' )
-    #    plt.xlabel(r'$\Delta \Sigma(R_{\rm p, i})$' )
+    ax = plt.subplot(3,3,1)
+    ax.plot(logMstel_arr, snr_arr,'-')
+    #ax.set_yscale('log')
+    ax.set_xlabel(r'$\log[M_*/{\rm h^{-1}M_\odot}]$')
+    ax.set_ylabel(r'${\rm SNR}$')
 
-    #    plt.savefig(outdir + 'corr-signals-fast_lmstelmin_%2.2f_lmstelmax_%2.2f.pdf'%(logMstelmin, logMstelmax), dpi=300)
-    #    plt.clf()
+    plt.savefig(pltdir + 'snr_paper.pdf', dpi=300)
+    return 0
 
-#logMstelarr = [9.5,10.0,10.5,11.0]
-logMstelarr = [9.5,11.0]
 
-for ii in range(9):
-    outdir = 'output/test_desi_z_0.1_0.4_seed_%d/'%ii
-    get_meas(outdir, logMstelarr)
-#make_plots(outdir, logMstelarr)
+#ss=17
+#logMmin =   11.20
+#logMmax =   11.30
+#outdir = 'output/desi_z_0.0_0.4/iso_centrals/%2.2f_%2.2f_seed_%d/'%(logMmin, logMmax, ss)
+#get_meas(outdir, [logMmin, logMmax], nover_samp=1000)
+#
 
 
 
 
-#for ii in range(10):
-#    outdir = 'output/test_desi_z_0.1_0.4_seed_%d/'%ii
-#    get_meas(outdir, logMstelarr)
-#make_plots(outdir, logMstelarr)
+logMstelarr = 9.5 + 0.1*np.arange(21)
+#for ss,(logMmin, logMmax) in enumerate(zip(logMstelarr[:-1], logMstelarr[1:])):
+#    #if logMmin!=11.5:
+#    #    continue
+#    outdir = 'output/desi_z_0.0_0.4/iso_centrals/%2.2f_%2.2f_seed_%d/'%(logMmin, logMmax, ss)
+#    get_meas(outdir, [logMmin, logMmax], nover_samp=1)
+
+##print('measurements done')
+make_plots('./plots/', logMstelarr)
+#plt.clf()
+#logMstelarr = logMstelarr[logMstelarr<=11.6]
+#plot_snr('./plots/', logMstelarr)
+
+## plot only first and last stellar mass bin
 
 
 
 
-
-
-
-#plt.subplot(2,2,2)
-#icov  = np.linalg.inv(xcov)*(Njacks - len(rbin) -2)/(Njacks -1)
-#chisq = np.dot(xdsigma, np.dot(icov, xdsigma))
-##chisq = sum(xdsigma**2 / xdsigmaerr**2)#np.dot(xdsigma, np.dot(icov, xdsigma))
-#from scipy.stats import chi2
-#print(rbin)
-#print(chisq, int(len(rbin)))
-#pval = chi2.sf(chisq,int(len(rbin)))
-#plt.errorbar(rbin*1e3, xdsigma*rbin, yerr=xdsigmaerr*rbin, fmt='.', capsize=3, label='%2.2f'%pval)
-#plt.axhline(0.0, color='black')
-#plt.ylabel(r'$R_{\rm p}\Delta \Sigma_{\times}$' )
-#plt.xlabel(r'${\rm R_{\rm p} [h^{-1}Kpc]}$')
-#plt.xscale('log')
